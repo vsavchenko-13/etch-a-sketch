7d978d20877cbf454ea25f805918c604f07a899a
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-04 00:15:51 -0600

    rewrote nested loop (simplified) and simplified flex boxes
    Have to rewrite hovering javascrip

--- js-file.js
+++ js-file.js
@@ -1,23 +1,16 @@
 let container = document.querySelector(".container");
-let squares = document.querySelectorAll(".horizontal");
 
-
-function grid(num) {
-    for (let i = 0; i < num; i++) {
-        let squareV = document.createElement("div");
-        squareV.classList.add("vertical");
-        container.appendChild(squareV);
-        for (let n = 0; n < num; n++) {
-            let squareH = document.createElement("div");
-            squareH.classList.add("horizontal");
-            squareH.addEventListener("mouseover", backgroundChange)
-            squareV.appendChild(squareH);
+function grid(num){
+    for (let i = 0; i < num; i++){
+        let vertical = document.createElement("div");
+        vertical.classList.add("vertical");
+        container.appendChild(vertical);
+        for(let n=0; n < num ; n++){
+            let horizontal = document.createElement("div");
+            horizontal.classList.add("horizontal");
+            vertical.appendChild(horizontal);
         }
     }
-}
-
-function backgroundChange(){
-    this.style.backgroundColor = "pink";
 }
 
 grid(16);
