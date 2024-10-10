e3a62762527c1f283ee84273f045f0f298adf4b6
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-09 18:52:50 -0600

    added function for resetting the grid
    Added if statements to make sure button functions correctly

--- js-file.js
+++ js-file.js
@@ -1,4 +1,6 @@
 let container = document.querySelector(".container");
+let button = document.querySelector("button");
+let vertical = document.querySelector(".vertical");
 
 function grid(num){
     for (let i = 0; i < num; i++){
@@ -18,4 +20,19 @@ function backgroundChange(){
     this.style.backgroundColor = "pink"
 }
 
+function resetGrid(){
+    let gridInt = prompt("how big do you want grid to be?");
+    gridInt = parseInt(gridInt,10);
+    if(!Number.isInteger(gridInt)){
+        gridInt = prompt("Please provide number!");
+    }
+    else if (gridInt > 100 || gridInt < 1){
+        gridInt = prompt("Please provide correct number!");
+    }
+    container.replaceChildren();
+    grid(gridInt);
+}
+
 grid(16);
+
+button.addEventListener("click", resetGrid);
